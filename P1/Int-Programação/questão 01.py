e0c0c9c92e1e1cb07902db6e13143c3cb1482b67
Keven-sn <kevendouglas.sn@gmail.com>
2024-09-30 13:03:57 -0300

    att

--- P1/Int-Programação/questão 01.py
+++ P1/Int-Programação/questão 01.py
@@ -2,4 +2,4 @@ x = int(input('Digite os passos do pirata para o Norte:'))
 y = int(input('Digite os passos do pirata para o Leste:'))
 
 passos = x + y
-print(f'Os passos cdo pirata são:{passos}')
+print(f'Os passos do pirata são:{passos}')
