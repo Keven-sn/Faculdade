532130422801066152ef15f7dba6b3009e106895
Keven-sn <kevendouglas.sn@gmail.com>
2024-11-04 21:06:36 -0300

    att

--- P1/Int-Programação/Aula 13/lista_de_compras.py
+++ P1/Int-Programação/Aula 13/lista_de_compras.py
@@ -1,4 +1,26 @@
 lista_de_compras = []
+
+
+def adicionar_item(lista_de_compras: list) -> list:
+    novo_item = (input('Digite o novo item desejado:'))
+    lista_de_compras = lista_de_compras + [novo_item]
+
+    return lista_de_compras
+
+
+def deletar_item(lista_de_compras: list) -> list:
+
+    item_remover = int(input('Digite o código do item:')) - 1
+    del lista_de_compras[item_remover]
+
+    return lista_de_compras
+
+
+def exibir_lista(lista_de_compras: list):
+    for i in range(len(lista_de_compras)):
+        print(f'{i+1} - {lista_de_compras}')
+
+
 opcao = 1000
 while opcao != 0:
     print('\n ================')
@@ -10,21 +32,19 @@ while opcao != 0:
 
     if opcao == 1:
         print("\n ====> ADICIONAR ITEM <====\n ")
-        novo_item = (input('Digite o novo item desejado:'))
-        lista_de_compras = lista_de_compras + [novo_item]
+        adicionar_item(lista_de_compras=lista_de_compras)
 
     elif opcao == 2:
         print("n ====>  REMOVER ITEM <====\n  ")
 
-        for i in range(len(lista_de_compras)):
-            print(f'{i+1} - {lista_de_compras}')
+        exibir_lista(lista_de_compras=lista_de_compras)
 
-            print('\n ===')
-            item_remover = int(input('Digite o código do item:')) - 1
-            del lista_de_compras[item_remover]
+        print('\n ===')
+        deletar_item(lista_de_compras=lista_de_compras)
 
     elif opcao == 3:
         print("\n ====> LISTA COMPLETA <====\n ")
 
         for i in range(len(lista_de_compras)):
             print(f'{i+1} - {lista_de_compras}')
+            exibir_lista(lista_de_compras=lista_de_compras)
