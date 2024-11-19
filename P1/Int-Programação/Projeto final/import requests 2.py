7ff7085a9cbac7907004f96daa579424fc010dc3
Keven-SN <kevendouglas.sn@gmail.com>
2024-11-18 21:11:34 -0300

    att

---
+++ P1/Int-Programação/Projeto final/import requests 2.py
@@ -0,0 +1,16 @@
+import requests
+
+from loguru import logger
+
+try:
+    for i in range(5):
+    
+        result= requests.get('https://api.adviceslip.com/advice')
+    
+        print(result.json())
+        print(result.json()['slip'])
+        print(result.json()['slip']['id'])
+        print(result.json()['slip']['advice'])
+    
+except Exception as error:
+    logger.exception(f'Error:{error}')
