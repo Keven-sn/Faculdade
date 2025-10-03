d440e1c01fa09f1d5abe49eb6476387c401bf70b
Keven <kevendouglas.sn@gmail.com>
2025-10-03 20:46:29 -0300

    Add files via upload

---
+++ P3/PadroesProjeto/ProjetoCadrastroPessoa/src/Funcionario.java
@@ -0,0 +1,9 @@
+import Pessoas.Pessoa;
+
+public class Funcionario extends Pessoa {
+    private int matricula;
+    private String cargo;
+    private double salario;
+    private String dataAdmissao;
+
+}
