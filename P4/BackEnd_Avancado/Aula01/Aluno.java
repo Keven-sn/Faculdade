68b9fccf29486d2b00e5bb2f1ebcce4f1df83142
Keven-SN <kevendouglas.sn@gmail.com>
2026-03-30 20:17:59 -0300

    bug

--- P4/BackEnd_Avancado/Aula01/Aluno.java
+++ P4/BackEnd_Avancado/Aula01/Aluno.java
@@ -1,4 +1,13 @@
 package BackEnd_Avancado.Aula01;
 
 public class Aluno {
+    private int Idade;
+    private String Nome;
+
+    public Aluno(int idade, String nome){
+     this.Idade = idade;
+     this.Nome = nome;
+    }
+    
+
 }
